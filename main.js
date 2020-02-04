8bac5e61560ec3b072c924a93bc93a813ed53308
Ryan Graham <ryangrahamwpb@gmail.com>
2020-02-04 13:39:13 -0500

    Get images and style customer block.

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-
+let row = document.querySelector(".row");
+for (customer of customers) {
+  let parent = document.createElement("div");
+  row.appendChild(parent);
+  let cusImg = document.createElement("img");
+  console.log(cusImg);
+  cusImg.src = customer.picture.large;
+  parent.classList.add("customer-block");
+  console.log(cusImg);
+  parent.appendChild(cusImg);
+}
